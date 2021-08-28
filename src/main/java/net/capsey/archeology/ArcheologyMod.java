dce36ffdd758411c209c19a6c4f61b50389b8347
CapsLock <aslan170505@gmail.com>
2021-08-28 21:27:59 +0600

    Mod info setup

---
+++ src/main/java/net/capsey/archeology/ArcheologyMod.java
@@ -0,0 +1,11 @@
+package net.capsey.archeology;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArcheologyMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+    
+}
