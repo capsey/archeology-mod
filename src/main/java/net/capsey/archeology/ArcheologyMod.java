e4c1dd19c19e1c11f05517a02e8778338d930ebe
CapsLock <aslan170505@gmail.com>
2021-08-29 01:08:00 +0600

    Copper Brush item added

--- src/main/java/net/capsey/archeology/ArcheologyMod.java
+++ src/main/java/net/capsey/archeology/ArcheologyMod.java
@@ -1,11 +1,18 @@
 package net.capsey.archeology;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ArcheologyMod implements ModInitializer {
 
+    public static final Item COPPER_BRUSH = new Item(new Item.Settings().group(ItemGroup.TOOLS));
+
     @Override
     public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("archeology", "copper_brush"), COPPER_BRUSH);
     }
     
 }
