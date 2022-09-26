55f3bed7dc2e97972283ac95f9e4ad16465d6d3e
Caps Lock <46106832+capsey@users.noreply.github.com>
2022-09-26 23:17:04 +0600

    Excavation Blocks underneath structures added
    Also default config values changed

--- src/main/java/net/capsey/archeology/ModConfig.java
+++ src/main/java/net/capsey/archeology/ModConfig.java
@@ -6,8 +6,8 @@ public class ModConfig extends MidnightConfig {
 
     @Client @Entry public static boolean disableBrushingAnimation = false;
     @Client @Entry public static boolean releaseUseKeyAfterBrushing = false;
-    @Client @Entry(min=0.0F, max=1.0F) public static float brushingLowerThreshold = 0.1F;
-    @Client @Entry public static float brushingBreakingSpeed = 1.0F;
+    @Client @Entry(min=0.0F, max=1.0F) public static float brushingLowerThreshold = 0.03F;
+    @Client @Entry public static float brushingBreakingSpeed = 3.0F;
     @Client @Entry public static float brushingRepairSpeed = 1.0F;
 
 }
