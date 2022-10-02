4333b17f5de943bb3a3f16698e95be60ecad0cfc
Caps Lock <46106832+capsey@users.noreply.github.com>
2022-10-02 23:05:02 +0600

    Excavation Sand block added

--- src/main/java/net/capsey/archeology/main/Blocks.java
+++ src/main/java/net/capsey/archeology/main/Blocks.java
@@ -21,9 +21,11 @@ public class Blocks {
     public static final TagKey<Block> EXCAVATION_BLOCKS_TAG = TagKey.of(Registry.BLOCK_KEY, new Identifier(ArcheologyMod.MOD_ID, "excavation_blocks"));
     public static final TagKey<Block> CLAY_POTS_TAG = TagKey.of(Registry.BLOCK_KEY, new Identifier(ArcheologyMod.MOD_ID, "clay_pots"));
     public static final TagKey<Block> CLAY_POT_PLANTABLE_TAG = TagKey.of(Registry.BLOCK_KEY, new Identifier(ArcheologyMod.MOD_ID, "clay_pot_plantable"));
+
     // Blocks
     public static Block EXCAVATION_DIRT;
     public static Block EXCAVATION_GRAVEL;
+    public static Block EXCAVATION_SAND;
     public static Block RAW_CLAY_POT;
     public static Block CLAY_POT;
     public static Block[] CLAY_POT_DYED;
@@ -31,6 +33,7 @@ public class Blocks {
     public static void onInitialize() {
         EXCAVATION_DIRT = register("excavation_dirt", new ExcavationBlock(AbstractBlock.Settings.copy(net.minecraft.block.Blocks.DIRT).hardness(1.0F)));
         EXCAVATION_GRAVEL = register("excavation_gravel", new FallingExcavationBlock(AbstractBlock.Settings.copy(net.minecraft.block.Blocks.GRAVEL).hardness(1.2F), (FallingBlock) net.minecraft.block.Blocks.GRAVEL));
+        EXCAVATION_SAND = register("excavation_sand", new FallingExcavationBlock(AbstractBlock.Settings.copy(net.minecraft.block.Blocks.SAND).hardness(1.0F), (FallingBlock) net.minecraft.block.Blocks.SAND));
         RAW_CLAY_POT = register("raw_clay_pot", new RawClayPotBlock(AbstractBlock.Settings.copy(net.minecraft.block.Blocks.CLAY).nonOpaque()));
 
         CLAY_POT = register("clay_pot", new ClayPotBlock(null, AbstractBlock.Settings.copy(net.minecraft.block.Blocks.TERRACOTTA).nonOpaque().strength(0.6F).sounds(ClayPotBlock.SOUND_GROUP)));
